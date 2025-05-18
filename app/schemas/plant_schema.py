92f055ece1a8a44809e0ddacabb96c667afcd0e1
하태영 <hottae00311@naver.com>
2025-05-19 01:39:22 +0900

    plant_schema.py 구조수정

--- app/schemas/plant_schema.py
+++ app/schemas/plant_schema.py
@@ -1,9 +1,17 @@
 from pydantic import BaseModel
 from typing import Optional
 
-# 데이터를 어떻게 저장할 건지 나태는 칸! 필요한 거 미리미리 정해두자
+# 응답이랑 전송 해는 거 구분하기.
 
 class PlantCreate(BaseModel):
     name: str
     memo: Optional[str] = ""
-    watering_cycle = int
+    watering_cycle: int
+
+class PlantResponse(BaseModel):
+    id: str
+    name: str
+    memo: Optional[str]
+    watering_cycle: int
+    user_id: str
+    created_at: str
